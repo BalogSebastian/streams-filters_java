b0b5889d6260c9385ad8b59173d7ff0ac4dddcff
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-03-31 11:27:58 +0200

    The first example.

--- stream_filters_java/src/main/java/countries/Main.java
+++ stream_filters_java/src/main/java/countries/Main.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Country>
+        List<Country> countries = new CountryRepository().getAll();
+
+        //System.out.println(countries);
+        feladat1(countries);
+
     }
+
+    public static void feladat1(List<Country> countries){
+        countries.stream()
+                .map(Country::name).forEach(System.out::println);
+    }
+
+
+
+
 
 }
